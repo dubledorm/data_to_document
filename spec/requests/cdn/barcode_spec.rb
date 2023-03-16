9f2b7afa05ef6840cc96290e364d6309c91ed6af
ubuntu <dubledorm@gmail.com>
2023-03-16 11:03:39 -0400

    Тесты для /cdn/barcode_to_image/{barcode}

--- spec/requests/cdn/barcode_spec.rb
+++ spec/requests/cdn/barcode_spec.rb
@@ -1,9 +1,9 @@
 require 'swagger_helper'
+require 'swagger_descriptions'
 
 RSpec.describe 'cdn/barcode', type: :request do
 
   path '/cdn/barcodes/{barcode}' do
-    # You'll want to customize the parameter types...
     parameter name: 'barcode', in: :path, type: :string, description: 'barcode'
 
     get('Получить Png файл с картинкой штрих кода') do
@@ -11,17 +11,24 @@ RSpec.describe 'cdn/barcode', type: :request do
       description BARCODE_TO_IMAGE_DESCRIPTION_CDN
       produces 'application/json'
       parameter name: :barcode, in: :path, type: :string, description: 'Штрих код для отображения'
-      response(200, 'successful') do
-        # let(:barcode) { '123' }
-        #
-        # after do |example|
-        #   example.metadata[:response][:content] = {
-        #     'application/json' => {
-        #       example: JSON.parse(response.body, symbolize_names: true)
-        #     }
-        #   }
-        # end
-        run_test!
+
+      response(200, 'Stream png file') do
+        let(:barcode) { '1234567890' }
+        run_test! do |response|
+          expect(response.body).to eq(File.open('spec/fixtures/barcode_1234567890.png', 'rb').read)
+        end
+      end
+
+      response(400, 'arguments error. Invalid barcode value') do
+        let(:barcode) { '1' }
+        example 'application/json', 'Invalid barcode value', {
+          message: 'data not valid'
+        }
+
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          expect(data['message']).to eq('data not valid')
+        end
       end
     end
   end
